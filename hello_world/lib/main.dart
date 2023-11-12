a475d527f2904fa5427cc0aa46d7b0f539b3e4b4
wnsdnn <a24369029@gmail.com>
2023-11-12 23:30:23 +0900

    hello world

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+
+
