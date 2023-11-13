15ba75d28223927f95e85e3dc2c316c474210248
wnsdnn <a24369029@gmail.com>
2023-11-13 23:34:18 +0900

    MainAxisAlignment 속성 알아보기

--- project/row_and_column/lib/screen/home_screen.dart
+++ project/row_and_column/lib/screen/home_screen.dart
@@ -10,7 +10,15 @@ class HomeScreen extends StatelessWidget {
         bottom: false,
         child: Container(
           color: Colors.black,
-          child: Column(
+          child: Row(
+            // MainAxisAlignment - 주축 정렬
+            // start - 시작
+            // end - 끝
+            // center - 가운데
+            // spaceBetween - 위젯과 위젯의 사이가 동일하게 배치된다.
+            // spaceEvenly - 위젯을 같은 간격으로 배치하지만 끝과 끝에도 위젯이 아닌 빈 간격으로 시작한다.
+            // spaceAround - spaceEvenly + 끝과 끝의 간격은 1/2
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
                 width: 50.0,
