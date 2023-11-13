7923879ba59b7cb48ed25f8ce95c9b0046846fb5
wnsdnn <a24369029@gmail.com>
2023-11-13 23:19:09 +0900

    row_and_column init

---
+++ project/row_and_column/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello Word'),
+      ),
+    );
+  }
+}
