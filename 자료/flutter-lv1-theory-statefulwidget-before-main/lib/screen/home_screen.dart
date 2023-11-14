4f9ce3e6dc7ecf2634749ec5b6f6c8c4edd0c432
wnsdnn <a24369029@gmail.com>
2023-11-14 22:48:05 +0900

    StatelessWidget -> StatefulWidget으로 변경해보기

--- 자료/flutter-lv1-theory-statefulwidget-before-main/lib/screen/home_screen.dart
+++ 자료/flutter-lv1-theory-statefulwidget-before-main/lib/screen/home_screen.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   final Color color;
+
   const HomeScreen({
     required this.color,
-    Key? key
+    Key? key,
   }) : super(key: key);
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 50.0,
       height: 50.0,
-      color: widget.color,
+      color: color,
     );
   }
 }
