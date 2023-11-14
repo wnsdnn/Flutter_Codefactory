c0ffa5f0530d75b757233b9b42f42ae4e4b58c53
wnsdnn <a24369029@gmail.com>
2023-11-15 00:12:48 +0900

    timer 추가

--- project/image_carousel/lib/screen/home_screen.dart
+++ project/image_carousel/lib/screen/home_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,10 +10,55 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+  PageController controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    timer = Timer.periodic(Duration(seconds: 3), (timer) {
+      int currentPage = controller.page!.toInt();
+
+      int nextPage = currentPage + 1;
+
+      if (nextPage > 4) {
+        nextPage = 0;
+      }
+
+      controller.animateToPage(
+        nextPage,
+        duration: Duration(milliseconds: 400),
+        curve: Curves.linear,
+      );
+    });
+  }
+
+  @override
+  // 생명주기에 마지막
+  void dispose() {
+    // controller 지우기
+    controller.dispose();
+
+    // timer 지우기
+    if (timer != null) {
+      timer?.cancel();
+    }
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // 상태바 색상 변경
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark); // 검정
+    // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light); // 흰색
+
     return Scaffold(
       body: PageView(
+        controller: controller,
         children: [1, 2, 3, 4, 5]
             .map(
               (e) => Image.asset(
