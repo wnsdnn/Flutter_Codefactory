28d657fc5e7d4db0c0dac319bfda19137829f29c
wnsdnn <a24369029@gmail.com>
2023-11-14 23:58:42 +0900

    PageView 사용해보기

--- 자료/flutter-lv1-project-image-carousel-main/lib/main.dart
+++ 자료/flutter-lv1-project-image-carousel-main/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:image_carousel/screen/home_screen.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     ),
   );
