a7b07a95f2e0ec50585bd449cb9accc99ad805de
wnsdnn <a24369029@gmail.com>
2023-11-15 09:44:44 +0900

    혼자서 따로 만들어 보기

---
+++ project/image_carousel2/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:image_carousel2/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
