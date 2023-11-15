44162e4691b1d8f9be4c0c7606a162c840e91af4
wnsdnn <a24369029@gmail.com>
2023-11-15 15:26:33 +0900

    화면 퍼블 완성

--- project/u_and_i/lib/screen/home_screen.dart
+++ project/u_and_i/lib/screen/home_screen.dart
@@ -14,48 +14,80 @@ class _HomeScreenState extends State<HomeScreen> {
       // 100 ~ 900까지 진하기 설정 가능
       backgroundColor: Colors.pink[100],
       body: SafeArea(
+        bottom: false,
         child: Container(
-          width: MediaQuery.of(context).size.width,
-          child: Column(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: [
+                _TopPart(),
+                _BottomPart(),
+              ],
+            )),
+      ),
+    );
+  }
+}
+
+class _TopPart extends StatelessWidget {
+  const _TopPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Expanded로 감싸서 크기가 넘어가는 오류 방지
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'U&I',
+            style: TextStyle(
+                color: Colors.white, fontFamily: 'parisienne', fontSize: 80.0),
+          ),
+          Column(
             children: [
-              Text(
-                'U&I',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'parisienne',
-                    fontSize: 80.0),
-              ),
               Text(
                 '우리 처음 만난날',
                 style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'sunflower',
-                    fontSize: 30.0),
+                    color: Colors.white, fontFamily: 'sunflower', fontSize: 30.0),
               ),
               Text(
                 '2021,12,27',
                 style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'sunflower',
-                    fontSize: 20.0),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.favorite),
-              ),
-              Text(
-                'D+1',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sunflower',
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.w700
-                ),
+                    color: Colors.white, fontFamily: 'sunflower', fontSize: 20.0),
               ),
             ],
           ),
-        ),
+          IconButton(
+            iconSize: 60.0,
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.red,
+            ),
+          ),
+          Text(
+            'D+1',
+            style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'sunflower',
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Expanded로 감싸서 크기가 넘어가는 오류 방지
+    return Expanded(
+      child: Image.asset('asset/img/middle_image.png'),
     );
   }
 }
