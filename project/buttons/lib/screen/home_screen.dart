004058815e74ec133710c6d4358c45601a2d24f2
wnsdnn <a24369029@gmail.com>
2023-11-17 00:17:06 +0900

    2:43

---
+++ project/buttons/lib/screen/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('버튼'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'ElevatedButton',
+              ),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('OutlinedButton'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
