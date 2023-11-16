de28aaa746a9d0fed7531418c328291a87f7c07b
wnsdnn <a24369029@gmail.com>
2023-11-16 15:56:53 +0900

    Slider 위젯 사용해보기

--- project/random_number_generator/lib/screen/settings_screen.dart
+++ project/random_number_generator/lib/screen/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -8,11 +9,58 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  double maxNumber = 10000;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Settings Screen'),
+      backgroundColor: PRIMARY_COLOR,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 10.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Row(
+                  children: maxNumber.toInt()
+                      .toString()
+                      .split('')
+                      .map(
+                        (e) => Image.asset(
+                          'asset/img/$e.png',
+                          width: 50.0,
+                          height: 70.0,
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+              Slider(
+                value: maxNumber,
+                min: 10000,
+                max: 1000000,
+                onChanged: (value) {
+                  // Slider는 버튼의 위치를 혼자서 못 바꿔줘서
+                  // value에다가 값을 넣어준뒤 다시 빌드 해줘야 버튼이 움직인다.
+                  setState(() {
+                    maxNumber = value;
+                  });
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: RED_COLOR,
+                ),
+                child: Text('저장!'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
