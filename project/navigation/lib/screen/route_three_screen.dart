a02f98ab47cf06f9c6bb5436de1865b3b7bd99c8
wnsdnn <a24369029@gmail.com>
2023-11-17 23:53:39 +0900

    named route 알아보기

--- project/navigation/lib/screen/route_three_screen.dart
+++ project/navigation/lib/screen/route_three_screen.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:navigation/layout/main_layout.dart';
 
 class RouteThreeScreen extends StatelessWidget {
-
   const RouteThreeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Route Three'),
-      ),
+    final argument = ModalRoute.of(context)!.settings.arguments;
+
+    return MainLayout(
+      title: 'Route Three',
+      children: [
+        Text(
+          'argument: ${argument}',
+          textAlign: TextAlign.center,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Pop'),
+        ),
+      ],
     );
   }
 }
