f2cd2e2a1ffb910079204af9a60fe0f457b82821
wnsdnn <a24369029@gmail.com>
2023-11-18 17:18:05 +0900

    다른 pop 메소드 알아보

--- project/navigation/lib/screen/home_screen.dart
+++ project/navigation/lib/screen/home_screen.dart
@@ -12,24 +12,55 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return MainLayout(
-      title: 'Home Screen',
-      children: [
-        ElevatedButton(
-          onPressed: () async {
-            final result = await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) {
-                  return RouteOneScreen(number: 123);
-                },
-              ),
-            );
+    return WillPopScope(
+      onWillPop: () async {
+        // true - 시스템 뒤로가기 활성화
+        // false - 시스템 뒤로가기 비활성화
+        // pop()을 함수는 못막음
 
-            print(result);
-          },
-          child: Text('Push'),
-        ),
-      ],
+        // 작업실행
+        final canPop = Navigator.of(context).canPop();
+        return canPop;
+      },
+      child: MainLayout(
+        title: 'Home Screen',
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              // [HomeScree()] => []
+              // 하나 밖에 없던 화면이 없어져서 검은 화면이 뜸
+              Navigator.of(context).pop();
+            },
+            child: Text('Pop'),
+          ),
+          ElevatedButton(onPressed: () {
+            // pop 가능: true
+            // pop 불가: false
+            print(Navigator.of(context).canPop());
+          }, child: Text('Can Pop'),),
+          ElevatedButton(
+            onPressed: () {
+              // 네이게이션 스택에 값이 1개 밖에 없으면 pop 안해줌
+              Navigator.of(context).maybePop();
+            },
+            child: Text('Maybe Pop'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final result = await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) {
+                    return RouteOneScreen(number: 123);
+                  },
+                ),
+              );
+
+              print(result);
+            },
+            child: Text('Push'),
+          ),
+        ],
+      ),
     );
   }
 }
