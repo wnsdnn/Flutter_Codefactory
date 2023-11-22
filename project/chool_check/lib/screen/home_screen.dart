36c899754ab582aeef1bf794e22edd3dc660ee17
wnsdnn <a24369029@gmail.com>
2023-11-22 23:52:05 +0900

    앱 권한 관리하기

--- project/chool_check/lib/screen/home_screen.dart
+++ project/chool_check/lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -35,6 +36,38 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+
+  Future<String> checkPermission() async {
+    // 기기에 위치 정보가 꺼져있는지 확인
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if(!isLocationEnabled) {
+      return '위치 서비스를 활성화 해주세요.';
+    }
+
+    // 현재 앱이 가지고 있는 위치 서비스에 대한 권한 가져오기
+
+    // denied - 앱 실행했을때 상태(기본)
+    // deniedForever - 권한 요청 거절 (다시는 이 앱에서 권한 요청을 못함)
+    // whilInUse - 권한 허용 (앱 실행중일때만)
+    // always - 권한 허용
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if(checkedPermission == LocationPermission.denied) {
+      checkedPermission = await Geolocator.requestPermission();
+
+      if(checkedPermission == LocationPermission.denied) {
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+
+    if(checkedPermission == LocationPermission.deniedForever) {
+      return '앱의 위치 권한을 세팅에서 허가해주세요.';
+    }
+
+    return '위치 권한이 허가되었습니다.';
+  }
+
 
   AppBar renderAppBar() {
     return AppBar(
