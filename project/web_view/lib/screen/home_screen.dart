bb331c14c68040a8f7638e5d2e673cfe569e79a8
wnsdnn <a24369029@gmail.com>
2023-11-27 23:42:50 +0900

    javascriptMode 활성화 및  webview 강의 끝

--- project/web_view/lib/screen/home_screen.dart
+++ project/web_view/lib/screen/home_screen.dart
@@ -7,6 +7,8 @@ final homeUrl = Uri.parse('https://wnsdnn.github.io/');
 class HomeScreen extends StatelessWidget {
   // .. => 뒤에 있는 메소드를 실행하기 하나 반환하는값은 실행한 함수의 대상(WebViewController)을 반환한다.
   WebViewController controller = WebViewController()
+    // setJavaScriptMode 자바스크립트 실행가능하게 해주기 (안드로이드는 기본이 비활성화라서 활성화 해줘야함)
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
     ..loadRequest(homeUrl);
 
   HomeScreen({super.key});
