30431a50e038d64e6049380807c8503c2ea7d98c
wnsdnn <a24369029@gmail.com>
2023-12-03 22:08:08 +0900

    프로젝트 테이블 구조

---
+++ project/calendar_scheduler/lib/model/schedule.dart
@@ -0,0 +1,24 @@
+import 'package:drift/drift.dart';
+
+class Schedules extends Table {
+  // PRIMARY KEY
+  IntColumn get id => integer()();
+
+  // 내용
+  TextColumn get content => text()();
+
+  // 일정 날짜
+  DateTimeColumn get date => dateTime()();
+
+  // 시작 시간
+  IntColumn get startTime => integer()();
+
+  // 끝 시간
+  IntColumn get endTime => integer()();
+
+  // Category Color Table Id
+  IntColumn get colorId => integer()();
+
+  // 생성날짜
+  DateTimeColumn get createdAt => dateTime()();
+}
