05a54a09fe9b034cb2347c24e7ad279d1996c555
wnsdnn <a24369029@gmail.com>
2023-12-04 00:27:53 +0900

    Flutter의 특별한 기능 - Code Generation

--- project/calendar_scheduler/lib/database/drift_database.dart
+++ project/calendar_scheduler/lib/database/drift_database.dart
@@ -8,10 +8,8 @@ import 'package:drift/native.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 
-
 // private 값까지 불러올 수 있다.
 part 'drift_database.g.dart';
-
 
 @DriftDatabase(
   tables: [
@@ -19,9 +17,19 @@ part 'drift_database.g.dart';
     CategoryColors,
   ],
 )
-
-class LocalDatabase extends _$LocalDataBase {
+class LocalDatabase extends _$LocalDatabase {
   LocalDatabase() : super(_openConnection());
+
+  Future<int> createSchedule(SchedulesCompanion data) =>
+      into(schedules).insert(data);
+
+  Future<int> createCategoryColor(CategoryColorsCompanion data) =>
+      into(categoryColors).insert(data);
+  
+  Future<List<CategoryColor>> getCategoryColors() => select(categoryColors).get();
+
+  @override
+  int get schemaVersion => 1;
 }
 
 LazyDatabase _openConnection() {
@@ -30,7 +38,5 @@ LazyDatabase _openConnection() {
     final file = File(p.join(dbFolder.path, 'db.sqlite'));
 
     return NativeDatabase(file);
-  })
+  });
 }
-
-
