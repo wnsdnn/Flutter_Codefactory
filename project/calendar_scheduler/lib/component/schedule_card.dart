fb8d94e96825cb047a00487d5819608a05786c6c
wnsdnn <a24369029@gmail.com>
2023-12-03 15:27:51 +0900

    ScheduleCard 설계

--- project/calendar_scheduler/lib/component/schedule_card.dart
+++ project/calendar_scheduler/lib/component/schedule_card.dart
@@ -1,12 +1,131 @@
+import 'package:calendar_scheduler/const/color.dart';
 import 'package:flutter/material.dart';
 
 class ScheduleCard extends StatelessWidget {
-  const ScheduleCard({super.key});
+  final int startTime;
+  final int endTime;
+  final String content;
+  final Color color;
+
+  const ScheduleCard({
+    required this.startTime,
+    required this.endTime,
+    required this.content,
+    required this.color,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('schedule card'),
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1.0,
+          color: PRIMARY_COLOR,
+        ),
+        borderRadius: BorderRadius.circular(
+          8.0,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: IntrinsicHeight(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _Time(
+                startTime: startTime,
+                endTime: endTime,
+              ),
+              SizedBox(
+                width: 16.0,
+              ),
+              Expanded(
+                child: _Content(
+                  content: content,
+                ),
+              ),
+              SizedBox(
+                width: 16.0,
+              ),
+              _Category(
+                color: color,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Time extends StatelessWidget {
+  final int startTime;
+  final int endTime;
+
+  const _Time({
+    super.key,
+    required this.startTime,
+    required this.endTime,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      fontWeight: FontWeight.w700,
+      color: PRIMARY_COLOR,
+      fontSize: 16.0,
+    );
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '${startTime.toString().padLeft(2, '0')}:00',
+          style: textStyle,
+        ),
+        Text(
+          '${endTime.toString().padLeft(2, '0')}:00',
+          style: textStyle.copyWith(fontSize: 10.0),
+        ),
+      ],
+    );
+  }
+}
+
+class _Content extends StatelessWidget {
+  final String content;
+
+  const _Content({
+    super.key,
+    required this.content,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      content,
+    );
+  }
+}
+
+class _Category extends StatelessWidget {
+  final Color color;
+
+  const _Category({
+    super.key,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        shape: BoxShape.circle,
+      ),
+      width: 16.0,
+      height: 16.0,
     );
   }
 }
--- project/calendar_scheduler/lib/screen/home_screen.dart
+++ project/calendar_scheduler/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:calendar_scheduler/component/calendar.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
 import 'package:flutter/material.dart';
 
@@ -32,6 +33,16 @@ class _HomeScreenState extends State<HomeScreen> {
             TodayBanner(
               selectDay: selectedDay,
               scheduleCount: 3,
+            ),
+            SizedBox(height: 8.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ScheduleCard(
+                startTime: 8,
+                endTime: 11,
+                content: '프로그래밍 공부하기',
+                color: Colors.red,
+              ),
             ),
           ],
         ),
