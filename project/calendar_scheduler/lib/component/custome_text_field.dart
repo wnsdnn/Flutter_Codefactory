b2f83f0c94d7aca7b75be329a468fcc423e93709
wnsdnn <a24369029@gmail.com>
2023-12-03 20:17:27 +0900

    Wrap 위젯 사용하기

---
+++ project/calendar_scheduler/lib/component/custome_text_field.dart
@@ -0,0 +1,38 @@
+import 'package:calendar_scheduler/const/color.dart';
+import 'package:flutter/material.dart';
+
+class CustomeTextField extends StatelessWidget {
+  final String label;
+
+  const CustomeTextField({
+    super.key,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            color: PRIMARY_COLOR,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        SizedBox(
+          height: 8.0,
+        ),
+        TextField(
+          cursorColor: Colors.grey,
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            filled: true,
+            fillColor: Colors.grey[300],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- project/calendar_scheduler/lib/component/schedule_bottom_sheet.dart
+++ project/calendar_scheduler/lib/component/schedule_bottom_sheet.dart
@@ -1,3 +1,5 @@
+import 'package:calendar_scheduler/component/custome_text_field.dart';
+import 'package:calendar_scheduler/const/color.dart';
 import 'package:flutter/material.dart';
 
 class ScheduleBottomSheet extends StatelessWidget {
@@ -15,12 +17,121 @@ class ScheduleBottomSheet extends StatelessWidget {
         padding: EdgeInsets.only(
           bottom: bottomInset,
         ),
-        child: Column(
-          children: [
-            TextField()
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _Time(),
+              SizedBox(height: 16.0),
+              _Content(),
+              SizedBox(height: 16.0),
+              _ColorPicker(),
+              SizedBox(height: 8.0),
+              _SaveButton(),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _Time extends StatelessWidget {
+  const _Time({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: CustomeTextField(
+            label: '시작시간',
+          ),
+        ),
+        SizedBox(
+          width: 16.0,
+        ),
+        Expanded(
+          child: CustomeTextField(
+            label: '마감시간',
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Content extends StatelessWidget {
+  const _Content({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomeTextField(
+      label: '내용',
+    );
+  }
+}
+
+class _ColorPicker extends StatelessWidget {
+  const _ColorPicker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      // 가로 gap
+      spacing: 8.0,
+      // 세로 gap
+      runSpacing: 10.0,
+      children: [
+        renderColor(Colors.red),
+        renderColor(Colors.orange),
+        renderColor(Colors.yellow),
+        renderColor(Colors.green),
+        renderColor(Colors.blue),
+        renderColor(Colors.indigo),
+        renderColor(Colors.purple),
+      ],
+    );
+  }
+
+  Widget renderColor(Color color) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color,
+      ),
+      width: 32.0,
+      height: 32.0,
+    );
+  }
+}
+
+class _SaveButton extends StatelessWidget {
+  const _SaveButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              primary: PRIMARY_COLOR,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(4.0),
+              )
+            ),
+            child: Text(
+              '저장',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
