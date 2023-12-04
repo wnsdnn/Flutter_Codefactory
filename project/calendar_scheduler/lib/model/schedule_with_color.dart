ab8acde685dc1efec8070a5361f53ad835eab6d7
wnsdnn <a24369029@gmail.com>
2023-12-04 22:53:58 +0900

    Join 쿼리 사용해보기

---
+++ project/calendar_scheduler/lib/model/schedule_with_color.dart
@@ -0,0 +1,12 @@
+import 'package:calendar_scheduler/database/drift_database.dart';
+
+
+class ScheduleWithColor {
+  final Schedule schedule;
+  final CategoryColor categoryColor;
+
+  ScheduleWithColor({
+    required this.schedule,
+    required this.categoryColor,
+  });
+}
