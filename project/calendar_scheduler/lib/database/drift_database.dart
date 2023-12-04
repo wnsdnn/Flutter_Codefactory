9aa9a8990f005ad1e15c5551547693e2fb870601
wnsdnn <a24369029@gmail.com>
2023-12-04 21:47:09 +0900

    Stream으로 쿼리 결과값 받아오기

--- project/calendar_scheduler/lib/database/drift_database.dart
+++ project/calendar_scheduler/lib/database/drift_database.dart
@@ -28,6 +28,8 @@ class LocalDatabase extends _$LocalDatabase {
 
   Future<List<CategoryColor>> getCategoryColors() => select(categoryColors).get();
 
+  Stream<List<Schedule>> watchSchedules() => select(schedules).watch();
+
   @override
   int get schemaVersion => 1;
 }
--- project/calendar_scheduler/lib/screen/home_screen.dart
+++ project/calendar_scheduler/lib/screen/home_screen.dart
@@ -5,7 +5,9 @@ import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
 import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
 import 'package:calendar_scheduler/const/color.dart';
+import 'package:calendar_scheduler/database/drift_database.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -85,20 +87,27 @@ class _SchduleList extends StatelessWidget {
     return Expanded(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: ListView.separated(
-          itemCount: 100,
-          separatorBuilder: (context, index) {
-            // 각각의 위젯들 사이에 들어갈 위젯 리턴
-            return SizedBox(height: 8.0);
-          },
-          itemBuilder: (context, index) {
-            return ScheduleCard(
-              startTime: 8,
-              endTime: 11,
-              content: '프로그래밍 공부하기',
-              color: Colors.red,
+        child: StreamBuilder<List<Schedule>>(
+          stream: GetIt.I<LocalDatabase>().watchSchedules(),
+          builder: (context, snapshot) {
+            print(snapshot.data);
+
+            return ListView.separated(
+              itemCount: 100,
+              separatorBuilder: (context, index) {
+                // 각각의 위젯들 사이에 들어갈 위젯 리턴
+                return SizedBox(height: 8.0);
+              },
+              itemBuilder: (context, index) {
+                return ScheduleCard(
+                  startTime: 8,
+                  endTime: 11,
+                  content: '프로그래밍 공부하기',
+                  color: Colors.red,
+                );
+              },
             );
-          },
+          }
         ),
       ),
     );
