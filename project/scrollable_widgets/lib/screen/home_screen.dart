2d0a1fda0553e77a0cc6982aaa2f35b7821978f6
wnsdnn <a24369029@gmail.com>
2023-12-05 23:03:04 +0900

    프로젝트명 수정

---
+++ project/scrollable_widgets/lib/screen/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widgets/layout/main_layout.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'Home',
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
