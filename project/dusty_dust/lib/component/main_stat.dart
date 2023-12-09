f5b3e4556938061edd00059f29a7633be8fb4a39
wnsdnn <a24369029@gmail.com>
2023-12-09 23:57:41 +0900

    CategoryCard 제작하기

---
+++ project/dusty_dust/lib/component/main_stat.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class MainStat extends StatelessWidget {
+  // 미세먼지 / 초미세먼지 등
+  final String cateogry;
+
+  // 아이콘 위치 (경로)
+  final String imgPath;
+
+  // 오염 정보
+  final String level;
+
+  // 오염 수치
+  final String stat;
+
+  const MainStat({
+    super.key,
+    required this.cateogry,
+    required this.imgPath,
+    required this.level,
+    required this.stat,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final ts = TextStyle(
+      color: Colors.black,
+    );
+
+    return Column(
+      children: [
+        Text(
+          cateogry,
+          style: ts,
+        ),
+        SizedBox(height: 8.0,),
+        Image.asset(
+          imgPath,
+          width: 50.0,
+        ),
+        SizedBox(height: 8.0,),
+        Text(
+          level,
+          style: ts,
+        ),
+        Text(
+          stat,
+          style: ts,
+        )
+      ],
+    );
+  }
+}
--- project/dusty_dust/lib/screen/home_screen.dart
+++ project/dusty_dust/lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dusty_dust/component/main_app_bar.dart';
 import 'package:dusty_dust/component/main_drawer.dart';
+import 'package:dusty_dust/component/main_stat.dart';
 import 'package:dusty_dust/const/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -8,17 +9,79 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: primaryColor,
       drawer: MainDrawer(),
       body: CustomScrollView(
         slivers: [
           MainAppbar(),
+
+          // 일반위젯 사용하고 싶을때 사용
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Card(
+                  margin: EdgeInsets.symmetric(horizontal: 8.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16.0),
+                    ),
+                  ),
+                  color: lightColor,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: darkColor,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(16.0),
+                              topRight: Radius.circular(16.0),
+                            )),
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Text(
+                            '종류별 통계',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          MainStat(
+                            cateogry: '미세먼지',
+                            imgPath: 'asset/img/best.png',
+                            level: '최고',
+                            stat: '0㎍/m³',
+                          ),
+                          MainStat(
+                            cateogry: '미세먼지',
+                            imgPath: 'asset/img/best.png',
+                            level: '최고',
+                            stat: '0㎍/m³',
+                          ),
+                          MainStat(
+                            cateogry: '미세먼지',
+                            imgPath: 'asset/img/best.png',
+                            level: '최고',
+                            stat: '0㎍/m³',
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
-
