0742d47dea6996b4670d94cca38344196235f82c
wnsdnn <a24369029@gmail.com>
2023-12-10 20:53:30 +0900

    HourlyCard 정리하기

--- project/dusty_dust/lib/screen/home_screen.dart
+++ project/dusty_dust/lib/screen/home_screen.dart
@@ -1,9 +1,7 @@
-import 'package:dusty_dust/component/card_title.dart';
 import 'package:dusty_dust/component/category_card.dart';
+import 'package:dusty_dust/component/hourly_card.dart';
 import 'package:dusty_dust/component/main_app_bar.dart';
-import 'package:dusty_dust/component/main_card.dart';
 import 'package:dusty_dust/component/main_drawer.dart';
-import 'package:dusty_dust/component/main_stat.dart';
 import 'package:dusty_dust/const/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -26,55 +24,7 @@ class HomeScreen extends StatelessWidget {
               children: [
                 CategoryCard(),
                 SizedBox(height: 16.0),
-                MainCard(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      CardTitle(
-                        title: '시간별 미세먼지',
-                      ),
-                      Column(
-                        children: List.generate(
-                          24,
-                          (index) {
-                            final now = DateTime.now();
-                            final hour = now.hour;
-                            int currentHour = hour - index;
-
-                            if (currentHour < 0) {
-                              currentHour += 24;
-                            }
-
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 8.0, vertical: 4.0),
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    child: Text(
-                                        '${currentHour.toString().padLeft(2, '0')}시'),
-                                  ),
-                                  Expanded(
-                                    child: Image.asset(
-                                      'asset/img/good.png',
-                                      height: 20.0,
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: Text(
-                                      '좋음',
-                                      textAlign: TextAlign.right,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            );
-                          },
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+                HourlyCard(),
               ],
             ),
           ),
