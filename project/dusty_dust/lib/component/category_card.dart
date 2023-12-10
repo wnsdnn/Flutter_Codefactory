6e1d5490cf52006f9ca8dcdd9deccc8f20e631b1
wnsdnn <a24369029@gmail.com>
2023-12-10 14:32:25 +0900

    CategoryCard 정리하기

---
+++ project/dusty_dust/lib/component/category_card.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import '../const/colors.dart';
+import 'main_stat.dart';
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 160.0,
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(16.0),
+          ),
+        ),
+        color: lightColor,
+        child: LayoutBuilder(
+          builder: (context, constraint) {
+            final double width = constraint.maxWidth / 3;
+
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: darkColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(16.0),
+                        topRight: Radius.circular(16.0),
+                      )),
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Text(
+                      '종류별 통계',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    physics: PageScrollPhysics(),
+                    children: List.generate(
+                      6,
+                      (index) => MainStat(
+                        cateogry: '미세먼지 $index',
+                        imgPath: 'asset/img/best.png',
+                        level: '최고',
+                        stat: '0㎍/m³',
+                        width: width,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }
+        ),
+      ),
+    );
+  }
+}
--- project/dusty_dust/lib/component/main_stat.dart
+++ project/dusty_dust/lib/component/main_stat.dart
@@ -13,12 +13,16 @@ class MainStat extends StatelessWidget {
   // 오염 수치
   final String stat;
 
+  // 너비
+  final double width;
+
   const MainStat({
     super.key,
     required this.cateogry,
     required this.imgPath,
     required this.level,
     required this.stat,
+    required this.width,
   });
 
   @override
@@ -27,27 +31,31 @@ class MainStat extends StatelessWidget {
       color: Colors.black,
     );
 
-    return Column(
-      children: [
-        Text(
-          cateogry,
-          style: ts,
-        ),
-        SizedBox(height: 8.0,),
-        Image.asset(
-          imgPath,
-          width: 50.0,
-        ),
-        SizedBox(height: 8.0,),
-        Text(
-          level,
-          style: ts,
-        ),
-        Text(
-          stat,
-          style: ts,
-        )
-      ],
+    return SizedBox(
+      width: width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            cateogry,
+            style: ts,
+          ),
+          SizedBox(height: 8.0,),
+          Image.asset(
+            imgPath,
+            width: 50.0,
+          ),
+          SizedBox(height: 8.0,),
+          Text(
+            level,
+            style: ts,
+          ),
+          Text(
+            stat,
+            style: ts,
+          )
+        ],
+      ),
     );
   }
 }
