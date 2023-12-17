cd1da2261727765168f0246c144c200f2d3e8ee8
wnsdnn <a24369029@gmail.com>
2023-12-17 22:56:19 +0900

    수업준비

---
+++ project/tabbar_theory/lib/screen/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:tabbar_theory/screen/basic_appbar_tabbar_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return BasicAppbarTabbarScreen();
+                      },
+                    ),
+                  );
+                },
+                child: Text('Basic AppBar TabBar Screen'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
