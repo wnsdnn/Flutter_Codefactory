5c597147fa31b46c6717003091293f411b9cb31a
wnsdnn <a24369029@gmail.com>
2023-12-17 18:52:44 +0900

    어댑터 생성 및 적용

--- project/dusty_dust/lib/main.dart
+++ project/dusty_dust/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:dusty_dust/model/stat_model.dart';
+import 'package:dusty_dust/screen/home_screen.dart';
 import 'package:dusty_dust/screen/test_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -9,13 +11,21 @@ void main() async {
   await Hive.initFlutter();
   await Hive.openBox(testBox);
 
+  Hive.registerAdapter<StatModel>(StatModelAdapter());
+  Hive.registerAdapter<ItemCode>(ItemCodeAdapter());
+
+  for(ItemCode itemCode in ItemCode.values) {
+    await Hive.openBox(itemCode.name);
+  }
+
+
   runApp(
     MaterialApp(
       theme: ThemeData(
         fontFamily: 'sunflower',
       ),
-      // home: HomeScreen(),
-      home: TestScreen(),
+      home: HomeScreen(),
+      // home: TestScreen(),
     ),
   );
 }
--- project/dusty_dust/lib/model/stat_and_status_model.dart
+++ project/dusty_dust/lib/model/stat_and_status_model.dart
@@ -14,5 +14,4 @@ class StatAndStatusModel {
     required this.itemCode,
   });
 
-
 }
