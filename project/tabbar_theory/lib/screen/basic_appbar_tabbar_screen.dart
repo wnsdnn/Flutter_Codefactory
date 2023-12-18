039241b8c337d48ec94a865a7f3f99ed9312616a
wnsdnn <a24369029@gmail.com>
2023-12-18 23:15:30 +0900

    탭바 스타일링

--- project/tabbar_theory/lib/screen/basic_appbar_tabbar_screen.dart
+++ project/tabbar_theory/lib/screen/basic_appbar_tabbar_screen.dart
@@ -11,22 +11,55 @@ class BasicAppbarTabbarScreen extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: Text('BasicAppBarScreen'),
-          bottom: TabBar(
-            tabs: TABS
-                .map(
-                  (e) => Tab(
-                    icon: Icon(e.icon),
-                    child: Text(
-                      '${e.label}',
-                      overflow: TextOverflow.ellipsis,
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(100),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: TabBar(
+                    indicatorColor: Colors.red,
+                    indicatorWeight: 4.0,
+                    // tab - tab의 크기
+                    // label - 글자의 크기
+                    indicatorSize: TabBarIndicatorSize.tab,
+                    // isScrollable: true,
+                    labelColor: Colors.yellow,
+                    unselectedLabelColor: Colors.black,
+                    labelStyle: TextStyle(
+                      fontWeight: FontWeight.w700,
                     ),
+                    unselectedLabelStyle: TextStyle(
+                        fontWeight: FontWeight.w300
+                    ),
+                    tabs: TABS
+                        .map(
+                          (e) => Tab(
+                        icon: Icon(e.icon),
+                        child: Text(
+                          '${e.label}',
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    )
+                        .toList(),
                   ),
                 )
-                .toList(),
+              ],
+            ),
           ),
         ),
-        body: Column(
-          children: [],
+        body: TabBarView(
+          // 스와이프 기능 막기
+          // physics: NeverScrollableScrollPhysics(),
+          children: TABS
+              .map(
+                (e) => Center(
+                  child: Icon(e.icon),
+                ),
+              )
+              .toList(),
         ),
       ),
     );
