20fcab7f56c7c96867734874b9bcb658d42adf3c
wnsdnn <a24369029@gmail.com>
2023-12-20 22:06:14 +0900

    실습

--- project/2nd/row_and_column/lib/screen/home_screen.dart
+++ project/2nd/row_and_column/lib/screen/home_screen.dart
@@ -6,66 +6,73 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
-        child: Container(
-          // width: MediaQuery.of(context).size.width,
-          color: Colors.black,
-          child: Column(
-            // MainAxisAlignment - 주축 정렬
-            // start - 시작
-            // end - 끝
-            // center - 가운데
-            // spaceBetween - 위젯과 위젯의 사이간격이 동일하게 배치된다.
-            // spaceEvenly - 위젯을 같은 간격으로 배치하지만 양끝에도 위젯이 아닌 빈 간격으로 시작한다.
-            // spaceAround - spaceEvenly + 양끝의 간격은 1/2
-            mainAxisAlignment: MainAxisAlignment.start,
-
-            // CrossAxisAlignment - 반대축 정렬
-            // start - 시작
-            // end - 끝
-            // center - 가운데
-            // stretch - 최대한으로 늘린다.
-            crossAxisAlignment: CrossAxisAlignment.start,
-
-            // MainAxisSize - 주축 크기
-            // max - 최대
-            // min - 최소
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              // Expanded / Flexible
-              // 남는공간 버리기
-              Flexible(
-                flex: 2,
-                child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
                   width: 50.0,
                   height: 50.0,
                   color: Colors.red,
                 ),
-              ),
-              Expanded(
-                child: Container(
+                Container(
                   width: 50.0,
                   height: 50.0,
                   color: Colors.orange,
                 ),
-              ),
-              Expanded(
-                child: Container(
+                Container(
                   width: 50.0,
                   height: 50.0,
                   color: Colors.yellow,
                 ),
-              ),
-              Expanded(
-                child: Container(
+                Container(
                   width: 50.0,
                   height: 50.0,
                   color: Colors.green,
                 ),
-              ),
-            ],
-          ),
-        ),
+              ],
+            ),
+            Container(
+              width: 50.0,
+              height: 50.0,
+              color: Colors.orange,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  width: 50.0,
+                  height: 50.0,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 50.0,
+                  height: 50.0,
+                  color: Colors.orange,
+                ),
+                Container(
+                  width: 50.0,
+                  height: 50.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 50.0,
+                  height: 50.0,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+            Container(
+              width: 50.0,
+              height: 50.0,
+              color: Colors.green,
+            ),
+          ],
+        )
       ),
     );
   }
