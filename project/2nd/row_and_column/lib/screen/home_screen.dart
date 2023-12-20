1a8540c97f174ecf6a7b5f6d564f9fc68fdcf6f2
wnsdnn <a24369029@gmail.com>
2023-12-20 21:42:47 +0900

    세팅하기

---
+++ project/2nd/row_and_column/lib/screen/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            children: [
+              Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.red,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.orange,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
