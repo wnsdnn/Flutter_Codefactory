83df4f8a702397c53a709a6d548b242268c50768
wnsdnn <a24369029@gmail.com>
2023-12-21 23:38:56 +0900

    디자인 마무리!

--- project/2nd/u_and_i/lib/screen/home_screen.dart
+++ project/2nd/u_and_i/lib/screen/home_screen.dart
@@ -13,24 +13,44 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.pink[100],
       body: SafeArea(
+        bottom: false,
         child: Container(
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
-              Text(
-                'U&I',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'parisienne',
-                    fontSize: 80.0,
-                    fontWeight: FontWeight.w700),
-              ),
+              _TopPart(),
+              _BottomPart(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TopPart extends StatelessWidget {
+  const _TopPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'U&I',
+            style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'parisienne',
+                fontSize: 80.0,
+                fontWeight: FontWeight.w700),
+          ),
+          Column(
+            children: [
               Text(
                 '우리 처음 만난날',
                 style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'sunflower',
-                    fontSize: 30.0),
+                    color: Colors.white, fontFamily: 'sunflower', fontSize: 30.0),
               ),
               Text(
                 '2021.12.27',
@@ -39,27 +59,40 @@ class _HomeScreenState extends State<HomeScreen> {
                   fontFamily: 'sunflower',
                   fontSize: 20.0,
                 ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.favorite,
-                  color: Colors.red,
-                ),
-              ),
-              Text(
-                'D+1',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sunflower',
-                  fontWeight: FontWeight.w700,
-                  fontSize: 50.0,
-                ),
               )
             ],
           ),
-        ),
+          IconButton(
+            iconSize: 60.0,
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.red,
+            ),
+          ),
+          Text(
+            'D+1',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'sunflower',
+              fontWeight: FontWeight.w700,
+              fontSize: 50.0,
+            ),
+          )
+        ],
       ),
     );
   }
 }
+
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Image.asset('asset/img/middle_image.png'),
+    );
+  }
+}
+
