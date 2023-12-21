9d2f7cdd7e4db1630b3f3a87445b60957f3833e9
wnsdnn <a24369029@gmail.com>
2023-12-21 22:27:13 +0900

    Life Cycle 확인하기

--- project/2nd/statefulwidget/lib/screen/home_screen.dart
+++ project/2nd/statefulwidget/lib/screen/home_screen.dart
@@ -6,21 +6,73 @@ class HomeScreen extends StatefulWidget {
   HomeScreen({
     super.key,
     required this.color,
-  });
+  }) {
+    print('Widget Constructor 실행!');
+  }
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomeScreen> createState() {
+    print('createState 실행!');
+
+    return _HomeScreenState();
+  }
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int number = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    print('initState 실행!');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    print('didChangeDepndencies 실행!');
+  }
+
+  @override
+  void deactivate() {
+    print('deactivate 실행!');
+
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print('despose 실행!');
+
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant HomeScreen oldWidget) {
+    print('didUpdateWidget 실행!');
+
+    super.didUpdateWidget(oldWidget);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          width: 50.0,
-          height: 50.0,
-          color: widget.color,
+    print('build 실행!');
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          print('setState 실행!');
+          number++;
+        });
+      },
+      child: Container(
+        width: 50.0,
+        height: 50.0,
+        color: widget.color,
+        child: Center(
+          child: Text(number.toString()),
         ),
       ),
     );
