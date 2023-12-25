d325b76321ca775235aa60096cbf3d01b2bf9dc5
wnsdnn <a24369029@gmail.com>
2023-12-25 12:16:48 +0900

    공부할 환경 세팅하기

---
+++ project/2nd/navigation/lib/layout/main_layout.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MainLayout extends StatelessWidget {
+  final String title;
+  final List<Widget> children;
+
+  const MainLayout({
+    super.key,
+    required this.title,
+    required this.children,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: children,
+        ),
+      ),
+    );
+  }
+}
---
+++ project/2nd/navigation/lib/screen/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:navigation/layout/main_layout.dart';
+import 'package:navigation/screen/route_one_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'Home Screen',
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) {
+                return RouteOneScreen();
+              },)
+            );
+          },
+          child: Text('Push'),
+        ),
+      ],
+    );
+  }
+}
