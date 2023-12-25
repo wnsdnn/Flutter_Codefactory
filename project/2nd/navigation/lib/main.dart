2415f3d6f507be3ba365897701e44defad11e577
wnsdnn <a24369029@gmail.com>
2023-12-25 13:15:13 +0900

    Named Route

--- project/2nd/navigation/lib/main.dart
+++ project/2nd/navigation/lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/screen/home_screen.dart';
+import 'package:navigation/screen/route_one_screen.dart';
+import 'package:navigation/screen/route_three_screen.dart';
+import 'package:navigation/screen/route_two_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: HomeScreen(),
+      // home: HomeScreen(),
+      initialRoute: '/',
+      // Named Route
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/one': (context) => RouteOneScreen(),
+        '/two': (context) => RouteTwoScreen(),
+        '/three': (context) => RouteThreeScreen(),
+      },
     ),
   );
 }
--- project/2nd/navigation/lib/screen/route_three_screen.dart
+++ project/2nd/navigation/lib/screen/route_three_screen.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:navigation/layout/main_layout.dart';
 
 class RouteThreeScreen extends StatelessWidget {
   const RouteThreeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Route Three'),
-      ),
+    final arguments = ModalRoute.of(context)!.settings.arguments;
+
+    return MainLayout(
+      title: 'Route Three',
+      children: [
+        Text(
+          'argument: ${arguments}',
+          textAlign: TextAlign.center,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Pop'),
+        ),
+      ],
     );
   }
 }
