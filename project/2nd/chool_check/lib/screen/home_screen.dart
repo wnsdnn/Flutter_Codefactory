5d62b216dc2b64cadd36381a0b8f4bcb1e144fbe
wnsdnn <a24369029@gmail.com>
2023-12-26 23:44:02 +0900

    앱 권한 관리하기

--- project/2nd/chool_check/lib/screen/home_screen.dart
+++ project/2nd/chool_check/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -34,6 +35,31 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+
+  Future<String> checkPermission() async {
+    // 위치권한이 설정되었는지 확인
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if(!isLocationEnabled) {
+      return '위치 서비스를 활성화 해주세요.';
+    }
+
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if(checkedPermission == LocationPermission.denied) {
+      checkedPermission = await Geolocator.requestPermission();
+
+      if(checkedPermission == LocationPermission.denied) {
+        return '위치 권한을 허가해주세요.';
+      }
+
+    } else if(checkedPermission == LocationPermission.deniedForever) {
+      return '앱의 위치 권한을 세팅에서 허가해주세요.';
+    }
+
+    return '위치 권한이 허가되었습니다.';
+  }
+
 
   AppBar renderAppBar() {
     return AppBar(
