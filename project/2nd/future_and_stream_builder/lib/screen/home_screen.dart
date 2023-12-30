a920881c2df6aed62395ea875f5deefc544bfeb0
wnsdnn <a24369029@gmail.com>
2023-12-31 00:39:07 +0900

    StreamBuilder 뽀개버렷!

--- project/2nd/future_and_stream_builder/lib/screen/home_screen.dart
+++ project/2nd/future_and_stream_builder/lib/screen/home_screen.dart
@@ -10,78 +10,125 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final textStyle = TextStyle(
+    fontSize: 16.0,
+  );
+
   @override
   Widget build(BuildContext context) {
-    final textStyle = TextStyle(
-      fontSize: 16.0,
-    );
-
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: FutureBuilder<int>(
-            future: getNumber(),
-            // future: null,
-            builder: (context, snapshot) {
-
-              // if (!snapshot.hasData) {
-              //   return Center(
-              //     child: CircularProgressIndicator(),
-              //   );
-              // }
-
-              if(snapshot.hasData) {
-                // 데이터가 있을떄 위젯 렌더링
-              }
-
-              if(snapshot.hasError) {
-                // 에러가 났을때 위젯 렌더링
-              }
-
-              // 로딩중일때 위젯 렌더링
-
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    'FutureBuilder',
-                    style: textStyle.copyWith(
-                        fontWeight: FontWeight.w700, fontSize: 20.0),
-                  ),
-                  Text(
-                    'ConState: ${snapshot.connectionState}',
-                    style: textStyle,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'Data: ${snapshot.data}',
-                        style: textStyle,
-                      ),
-                      if (snapshot.connectionState == ConnectionState.waiting)
-                        CircularProgressIndicator(),
-                    ],
-                  ),
-                  Text(
-                    'Error: ${snapshot.error}',
-                    style: textStyle,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {});
-                    },
-                    child: Text('setState'),
-                  ),
-                ],
-              );
-            },
-          ),
+          child: renderStreamBuilder(),
         ),
       ),
     );
   }
+
+  FutureBuilder renderFutureBuilder() {
+    return FutureBuilder<int>(
+      future: getNumber(),
+      // future: null,
+      builder: (context, snapshot) {
+        // if (!snapshot.hasData) {
+        //   return Center(
+        //     child: CircularProgressIndicator(),
+        //   );
+        // }
+
+        if (snapshot.hasData) {
+          // 데이터가 있을떄 위젯 렌더링
+        }
+
+        if (snapshot.hasError) {
+          // 에러가 났을때 위젯 렌더링
+        }
+
+        // 로딩중일때 위젯 렌더링
+
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'FutureBuilder',
+              style: textStyle.copyWith(
+                  fontWeight: FontWeight.w700, fontSize: 20.0),
+            ),
+            Text(
+              'ConState: ${snapshot.connectionState}',
+              style: textStyle,
+            ),
+            Row(
+              children: [
+                Text(
+                  'Data: ${snapshot.data}',
+                  style: textStyle,
+                ),
+                if (snapshot.connectionState == ConnectionState.waiting)
+                  CircularProgressIndicator(),
+              ],
+            ),
+            Text(
+              'Error: ${snapshot.error}',
+              style: textStyle,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {});
+              },
+              child: Text('setState'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  StreamBuilder renderStreamBuilder() {
+    return StreamBuilder<int>(
+      stream: streamNumbers(),
+      builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'StreamBuilder',
+              style: textStyle.copyWith(
+                  fontWeight: FontWeight.w700, fontSize: 20.0),
+            ),
+            Text(
+              'ConState: ${snapshot.connectionState}',
+              style: textStyle,
+            ),
+            Row(
+              children: [
+                Text(
+                  'Data: ${snapshot.data}',
+                  style: textStyle,
+                ),
+                if (snapshot.connectionState == ConnectionState.waiting)
+                  CircularProgressIndicator(),
+              ],
+            ),
+            Text(
+              'Error: ${snapshot.error}',
+              style: textStyle,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {});
+              },
+              child: Text('setState'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
 
   Future<int> getNumber() async {
     await Future.delayed(Duration(seconds: 3));
@@ -91,5 +138,18 @@ class _HomeScreenState extends State<HomeScreen> {
     // throw Exception('에러가 발생했습니다.');
 
     return random.nextInt(100);
+  }
+
+  Stream<int> streamNumbers() async* {
+    for (int i = 0; i < 10; i++) {
+
+      await Future.delayed(Duration(seconds: 1));
+
+      if(i == 5) {
+        throw Exception('i = 5');
+      }
+
+      yield i;
+    }
   }
 }
