affd5f97844eab1bbd5837894a9f8dd931fe0f35
wnsdnn <a24369029@gmail.com>
2023-12-31 23:21:22 +0900

    HomeScreen 설계

--- project/2nd/video_call/lib/const/agora.dart
+++ project/2nd/video_call/lib/const/agora.dart
@@ -2,5 +2,5 @@ const APP_ID = '606f7fd491ad430096d810e66c8808ff';
 
 const CHANNEL_NAME = 'testchannel';
 
-const TEMP_TOKEN = '007eJxTYOhz9Oav+31zrj4Tn/pK86mXX82/8DP9mp3NhNtrNy4VD7mkwGBmYJZmnpZiYmmYmGJibGBgaZZiYWiQamaWbGFhYJGWpmg7IbUhkJHBIjaOiZEBAkF8boaS1OKS5IzEvLzUHAYGADniIcA=';
+const TEMP_TOKEN = '007eJxTYJD+HpovkezKb7a+XfrhbWYjzo28R28++Lx4fXD5ga0T39orMJgZmKWZp6WYWBomppgYGxhYmqVYGBqkmpklW1gYWKSl/SidmNoQyMjQ/vI2EyMDBIL43AwlqcUlyRmJeXmpOQwMADN6I1E=';
 
