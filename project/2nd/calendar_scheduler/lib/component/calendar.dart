f6de0c7de9a37877eb5dde978121d3f78b4c6cc2
wnsdnn <a24369029@gmail.com>
2024-01-01 01:47:15 +0900

    Table Calendar 소개

---
+++ project/2nd/calendar_scheduler/lib/component/calendar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatefulWidget {
+  const Calendar({super.key});
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  DateTime? selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return TableCalendar(
+      focusedDay: DateTime.now(),
+      firstDay: DateTime(1800),
+      lastDay: DateTime(3000),
+      headerStyle: HeaderStyle(
+        formatButtonVisible: false,
+        titleCentered: true,
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 16.0,
+        ),
+      ),
+      onDaySelected: (selectedDay, focusedDay) {
+        print(selectedDay);
+        setState(() {
+          this.selectedDay = selectedDay;
+        });
+      },
+      selectedDayPredicate: (day) {
+        if(selectedDay == null) {
+          return false;
+        }
+
+        return day.year == selectedDay!.year &&
+            day.month == selectedDay!.month &&
+            day.day == selectedDay!.day;
+      },
+    );
+  }
+}
