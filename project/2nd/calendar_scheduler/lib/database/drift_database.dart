7cc8aabfcb13ae77a9224c46a51700bf4a463657
wnsdnn <a24369029@gmail.com>
2024-01-02 14:32:55 +0900

    데이터베이스 생성하기

---
+++ project/2nd/calendar_scheduler/lib/database/drift_database.dart
@@ -0,0 +1,41 @@
+// private 값들을 불러올 수 없다.
+import 'dart:io';
+
+import 'package:calendar_scheduler/model/category_color.dart';
+import 'package:calendar_scheduler/model/schedule.dart';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+
+// private 값까지 불러올 수 있다.
+// 같은 파일인데 코드가 많아서 나눠놓은 느낌으로 생각.
+part 'drift_database.g.dart';
+
+
+@DriftDatabase(
+  tables: [
+    Schedules,
+    CategoryColors,
+  ],
+)
+
+class LocalDatabase extends _$LocalDatabase {
+  LocalDatabase() : super(_openConnection());
+
+
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
+
+
+
+
+
