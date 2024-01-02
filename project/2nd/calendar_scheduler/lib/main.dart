eb953a09986509395217586484db1f93171bcfa4
wnsdnn <a24369029@gmail.com>
2024-01-02 15:33:22 +0900

    색상정보 DB에 넣기

--- project/2nd/calendar_scheduler/lib/main.dart
+++ project/2nd/calendar_scheduler/lib/main.dart
@@ -1,6 +1,25 @@
+import 'package:calendar_scheduler/database/drift_database.dart';
 import 'package:calendar_scheduler/screen/home_screen.dart';
+import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+
+const DEFAULT_COLORS = [
+  // 빨강
+  'F44336',
+  // 주황
+  'FF9800',
+  // 노랑
+  'FFEB3B',
+  // 초록
+  'FCAF50',
+  // 파랑
+  '2196F3',
+  // 남색
+  '3F51B5',
+  // 보라
+  '9C27B0',
+];
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,11 +27,27 @@ void main() async {
   // intl 패키지 초기화 시켜주기
   await initializeDateFormatting();
 
+
+  final database = LocalDatabase();
+  final colors = await database.getCategoryColors();
+
+  print('--------- GET COLORS ----------');
+  print(colors);
+
+  if (colors.isEmpty) {
+    for (String hexCode in DEFAULT_COLORS) {
+      await database.createCategoryColor(
+        CategoryColorsCompanion(
+          hexCode: Value(hexCode),
+        ),
+      );
+    }
+  }
+
+
   runApp(
     MaterialApp(
-      theme: ThemeData(
-        fontFamily: 'NotoSans'
-      ),
+      theme: ThemeData(fontFamily: 'NotoSans'),
       home: HomeScreen(),
     ),
   );
