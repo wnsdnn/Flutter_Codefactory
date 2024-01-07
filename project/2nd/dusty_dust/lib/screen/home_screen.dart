4692fb7e1ee64086ca23f1735b68a06523388e4d
wnsdnn <a24369029@gmail.com>
2024-01-07 23:50:04 +0900

    StatusModel 제작하기

--- project/2nd/dusty_dust/lib/screen/home_screen.dart
+++ project/2nd/dusty_dust/lib/screen/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:dusty_dust/component/main_drawer.dart';
 import 'package:dusty_dust/const/colors.dart';
 import 'package:dusty_dust/const/data.dart';
 import 'package:dusty_dust/model/stat_model.dart';
+import 'package:dusty_dust/repository/stat_repository.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -23,27 +24,9 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   fetchData() async {
-    final response = await Dio().get(
-      'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst',
-      queryParameters: {
-        'serviceKey': serviceKey,
-        'returnType': 'json',
-        'numOfRows': 30,
-        'pageNo': 1,
-        'itemCode': 'PM10',
-        'dataGubun': 'HOUR',
-        'searchCondition': 'WEEK',
-      },
-    );
+    final statModels = await StatRepository.fetchData();
 
-    response.data['response']['body']['items'].map(
-      (item) {
-        print(item);
-        return item;
-      },
-    );
-    print(response.data['response']['body']['items'].map((item) => StatModel.fromJson(json: item)));
-    // print(.map((item) => StatModel.fromJson(json: item)));
+    print(statModels);
   }
 
   @override
