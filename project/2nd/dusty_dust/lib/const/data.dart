bd5895acc3f5715bc37f47fb27e1472e7eb2d786
wnsdnn <a24369029@gmail.com>
2024-01-07 22:55:24 +0900

    Dio로API 테스트해보기

---
+++ project/2nd/dusty_dust/lib/const/data.dart
@@ -0,0 +1,1 @@
+const serviceKey = 'dE8obuGSDgBoHDsBzp9iUeqT9vN75iKZ+cUZpto65SbUJAmCnLEuN1VTBAhig3sRtq6TGShZ8IOuaFdppwMFog==';
--- project/2nd/dusty_dust/lib/screen/home_screen.dart
+++ project/2nd/dusty_dust/lib/screen/home_screen.dart
@@ -1,14 +1,43 @@
-import 'package:dusty_dust/component/card_title.dart';
+import 'package:dio/dio.dart';
 import 'package:dusty_dust/component/category_card.dart';
 import 'package:dusty_dust/component/hourly_card.dart';
 import 'package:dusty_dust/component/main_app_bar.dart';
-import 'package:dusty_dust/component/main_card.dart';
 import 'package:dusty_dust/component/main_drawer.dart';
 import 'package:dusty_dust/const/colors.dart';
+import 'package:dusty_dust/const/data.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    final response = await Dio().get(
+      'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst',
+      queryParameters: {
+        'serviceKey': serviceKey,
+        'returnType': 'json',
+        'numOfRows': 30,
+        'pageNo': 1,
+        'itemCode': 'PM10',
+        'dataGubun': 'HOUR',
+        'searchCondition': 'WEEK',
+      },
+    );
+
+    print(response.data);
+  }
 
   @override
   Widget build(BuildContext context) {
